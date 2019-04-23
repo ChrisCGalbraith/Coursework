dfb632d0aa508fa369fbaa06ec7193f51882bc06
Chris <chriscgalbraith@gmail.com>
2019-04-23 22:21:39 +0100

    Added duplicateMakeExceptions to addMake method. Redundant imports removed
    Signed-off-by: Chris <chriscgalbraith@gmail.com>

--- Struct-Algorithm/src/main/java/com/github/chriscgalbraith/structalgo/Dealership.java
+++ Struct-Algorithm/src/main/java/com/github/chriscgalbraith/structalgo/Dealership.java
@@ -5,68 +5,72 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Dealership {
-	
-	public class noMakeFoundException extends Exception{
-		
+
+	public class noMakeFoundException extends Exception {
+		private static final long serialVersionUID = 1L;
+	}
+
+	public class noModelFoundException extends Exception {
+		private static final long serialVersionUID = 1L;
 	}
 	
-	public class noModelFoundException extends Exception{
-		
+	public class duplicateMakeException extends Exception {
+		private static final long serialVersionUID = 1L;
 	}
-	
+
 	private TreeMap<String, TreeMap<String, ArrayList<Car>>> myDoubleTree = new TreeMap<>();
 	String make;
 	String model;
 	Car car;
-	
-	public void addMake(String make) {
-		
-		//Check if the make already exists
-		if(myDoubleTree.containsKey(make)) {
-			System.out.println("That make of car already exists.");
-		}
-		else {
+
+	public void addMake(String make) throws duplicateMakeException {
+
+		// Check if the make already exists
+		if (myDoubleTree.containsKey(make)) {
+				throw new duplicateMakeException();
+			
+		} else {
 			myDoubleTree.put(make, new TreeMap<>());
-			System.out.println("Successfuly added" + make + "to the Dealership.");
+			System.out.println("Successfuly added " + make + " to the Dealership.");
 		}
 	}
 	
-	
 	public void removeMake(String make) throws noMakeFoundException {
-		
-		//Check if the make exists in the tree
-		if(!myDoubleTree.containsKey(make)) {
+
+		// Check if the make exists in the tree
+		if (!myDoubleTree.containsKey(make)) {
 			throw new noMakeFoundException();
-		}
-		else {
+		} else {
 			myDoubleTree.remove(make);
 		}
 	}
-	
+
 	public void addCar(String make, String model, Car car) throws noMakeFoundException, noModelFoundException {
-		
+
 		// Check if the make of car exists in the system
-		if(!myDoubleTree.containsKey(make)) {
+		if (!myDoubleTree.containsKey(make)) {
 			System.out.println("The selected make does not exist");
 			throw new noMakeFoundException();
-		} 
-		
-	    // Check if the model of the car exists in the system
-		else if(!myDoubleTree.get(make).containsKey(model)) {
+		}
+
+		// Check if the model of the car exists in the system
+		else if (!myDoubleTree.get(make).containsKey(model)) {
 			System.out.println("The selected model does not exist");
 			throw new noModelFoundException();
 		}
-		
+
 		else {
 			myDoubleTree.get(make).get(model).add(car);
-	    }
-   }
-	
-	
+		}
+	}
+
 	public void displayMakes() {
-		for (Map.Entry<String, TreeMap<String, ArrayList<Car>>> entry : myDoubleTree.entrySet())
-	    {
-	      System.out.println(entry.getKey());
-	    }
+		if(myDoubleTree.size() == 0) {
+			System.out.println("There are no makes in the system to display.");
+		}
+		else 
+			for (Map.Entry<String, TreeMap<String, ArrayList<Car>>> entry : myDoubleTree.entrySet()) {
+			System.out.println(entry.getKey());
+		}
 	}
 }
--- Struct-Algorithm/src/main/java/com/github/chriscgalbraith/structalgo/DealershipTest.java
+++ Struct-Algorithm/src/main/java/com/github/chriscgalbraith/structalgo/DealershipTest.java
@@ -3,30 +3,46 @@ package com.github.chriscgalbraith.structalgo;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
+import com.github.chriscgalbraith.structalgo.Dealership.duplicateMakeException;
+import com.github.chriscgalbraith.structalgo.Dealership.noMakeFoundException;
+
 public class DealershipTest {
-	
+
 	public static void main(String[] args) {
 		Dealership dealership = new Dealership();
 		Integer option;
-		do{
-	            System.out.println("0: Quit");
-	            System.out.println("1: Add a new make of car");
-	            System.out.println("2: Display makes of car available");    //Check bookmark on arraylist
-	            option=Input.getInteger("option: ");
-	            switch (option){
-	                case 0:
-	                    System.out.println("quitting program");
-	                    break;
-	                case 1:
-	                	dealership.addMake(Input.getString("Enter the make of car to add to the Dealership"));
-	                	break;
-	                case 2:
-	                	dealership.displayMakes();
-	                	break;
-	                default:
-	                    System.out.println("invalid option");
-	            }
-	        }while(option!=0);
+		do {
+			System.out.println("0: Quit.");
+			System.out.println("1: Add a new make of car to the Dealership.");
+			System.out.println("2: Display makes of car available."); 
+			System.out.println("3: Remove a make of car from the Dealership."); 
+			option = Input.getInteger("option: ");
+			switch (option) {
+			case 0:
+				System.out.println("quitting program");
+				break;
+			case 1:
+				try {
+					dealership.addMake(Input.getString("Enter the make of car to add to the Dealership"));
+				} catch (duplicateMakeException e1) {
+					System.out.println("That make already exists in the system.");
+				}
+				break;
+			case 2:
+				dealership.displayMakes();
+				break;
+			case 3:
+				try {         // Try to remove make, if doesnt exist, handles exception and returns to top
+					dealership.removeMake(Input.getString("Enter the make to remove from the system"));
+				} 
+				catch (noMakeFoundException e) {
+					System.out.println("Exception: The make does not exist! Going back to main menu.");	
+				}
+				break;
+			default:
+				System.out.println("invalid option");
+			}
+		} while (option != 0);
 
 	}
 }
